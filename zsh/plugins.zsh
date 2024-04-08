62eb8947becd67f7fe82c038c49a8b287660df37
chanlettuce <37320700+chanlettuce@users.noreply.github.com>
2024-04-08 14:33:26 +0900

    Add git 'k'

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -3,7 +3,9 @@ zinit light romkatv/powerlevel10k
 zinit light zdharma/fast-syntax-highlighting
 zinit light zsh-users/zsh-autosuggestions
 bindkey '^j' autosuggest-accept
+
 zinit light paulirish/git-open
+zinit light supercrabtree/k
 
 zinit ice as"program" from"gh-r" mv"bat* -> bat" pick"bat/bat"
 zinit light sharkdp/bat
