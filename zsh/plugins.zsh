0eee50a01c5697266683871b5f906da148ae8922
chanlettuce <37320700+chanlettuce@users.noreply.github.com>
2024-04-08 00:39:01 +0000

    batの追加

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -4,3 +4,11 @@ zinit light zdharma/fast-syntax-highlighting
 zinit light zsh-users/zsh-autosuggestions
 bindkey '^j' autosuggest-accept
 zinit light paulirish/git-open
+
+zinit ice as"program" from"gh-r" mv"bat* -> bat" pick"bat/bat"
+zinit light sharkdp/bat
+
+# 以下はただのエイリアス設定
+if builtin command -v bat >/dev/null; then
+  alias cat="bat"
+fi
