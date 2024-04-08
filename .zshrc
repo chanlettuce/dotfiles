04fbb9dd89076250c7586d3416ad6ae42dfa9673
chanlettuce <37320700+chanlettuce@users.noreply.github.com>
2024-04-08 23:58:36 +0900

    zsh周りのファイルを整理

--- .zshrc
+++ .zshrc
@@ -27,7 +27,8 @@ source $SCRIPT_DIR/zsh/plugins.zsh
 source $SCRIPT_DIR/zsh/config.zsh
 source $SCRIPT_DIR/zsh/p10k.zsh
 source $SCRIPT_DIR/zsh/alias.zsh
-source $SCRIPT_DIR/zsh/check_update_dotfiles.zsh
+source $SCRIPT_DIR/zsh/functions.zsh
+source $SCRIPT_DIR/zsh/check_git_status.zsh
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
--- zsh/config.zsh
+++ zsh/config.zsh
@@ -28,32 +28,3 @@ setopt auto_cd
 
 # disable ctrl+s, ctrl+q
 setopt no_flow_control
-
-# マニュアル関数
-function mm() {
-  if [[ $1 ]]; then
-    printf "\033[36m%-20s\033[0m %-20s %-30s\n" "[main:sub]" "[Command]" "[Description]"
-    cat $HOME/dotfiles/.manurc | grep $1 | awk -F " *?## *?" '{printf "\033[36m%-20s\033[0m %-20s %-30s\n", $1, $2, $3}'
-  else
-    echo "mm iterm[:line, :tab, :window]"
-    echo "mm alias[:dir, ...]"
-  fi
-}
-
-# NPM Script
-function nsc() {
-  if [[ -f package.json ]]; then
-    printf "\033[36m%-44s\033[0m %-20s\n" "[Command]" "[Description]"
-    cat package.json | jq ".scripts" | grep : | sed -e 's/,//g' | awk -F "\": \"" '{printf "(npm run|yarn)\033[36m%-30s\033[0m %-20s\n", $1, $2}' | sed -e 's/\"//g'
-  fi
-}
-
-# CD Project-directory
-# プロジェクトのディレクトリに応じてlsコマンドの`*`を変更してください。
-function cdp() {
-  local dir="$(ls -1d $HOME/vsc/* | peco)"
-  if [ ! -z "$dir" ]; then
-    echo $dir
-    cd $dir
-  fi
-}
