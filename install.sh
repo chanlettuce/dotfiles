512c2b9fb8acd870225c057dfe25fc2ec75a9484
chanlettuce <37320700+chanlettuce@users.noreply.github.com>
2024-04-08 23:58:06 +0900

    色々インストールするように

---
+++ install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#for Ubuntu
+
+sudo apt update
+
+# bpytop
+if [ -z "$(command -v bpytop)" ]; then
+  echo "--- Install peco is Start! ---"
+  sudo apt -y install bpytop
+fi
+
+# exa
+if [ -z "$(command -v exa)" ]; then
+  echo "--- Install exa is Start! ---"
+  sudo apt -y install exa
+fi
+
+# peco
+if [ -z "$(command -v jq)" ]; then
+  echo "--- Install jq is Start! ---"
+  sudo apt -y install jq
+fi
+
+# peco
+if [ -z "$(command -v peco)" ]; then
+  echo "--- Install peco is Start! ---"
+  sudo apt -y install peco
+fi
+
+# zsh
+ln -sv ~/dotfiles/.zshrc ~/
+
+sudo apt -y install zsh
+chsh -s /usr/bin/zsh
