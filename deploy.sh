0e54a8cce9f4b01cc7342b35ed66fae780c1abd8
chanlettuce <37320700+chanlettuce@users.noreply.github.com>
2024-04-08 01:27:25 +0000

    Update deploy command

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-# 配置したい設定ファイル
-dotfiles=(.zshrc .tmux.conf)
+#for Ubuntu
+ln -sv ~/dotfiles/.zshrc ~/
 
-# .zshrc と .tmux.conf という設定ファイルのシンボリックリンクを
-# ホームディレクトリ直下に作成する
-for file in "${dotfiles[@]}"; do
-  ln -svf $file ~/
-done
+sudo apt install zsh
+chsh -s /usr/bin/zsh
