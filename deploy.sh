1cd70760a5436ca3a08e928f0a36232ade12c62f
chanlettuce <37320700+chanlettuce@users.noreply.github.com>
2024-04-08 16:38:27 +0900

    update deploy command

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 #for Ubuntu
+
+# peco
+if [ -z "$(command -v peco)" ]; then
+    echo "--- Install peco is Start! ---"
+    sudo apt -y install peco
+fi
+
+# zsh
 ln -sv ~/dotfiles/.zshrc ~/
 
-sudo apt install zsh
+sudo apt -y install zsh
 chsh -s /usr/bin/zsh
