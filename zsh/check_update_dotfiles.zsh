0f2d572ebc454efcdeac653d0d774f81a6f8904c
chanlettuce <37320700+chanlettuce@users.noreply.github.com>
2024-04-08 14:57:23 +0900

    update dotfiles

--- zsh/check_update_dotfiles.zsh
+++ zsh/check_update_dotfiles.zsh
@@ -1,9 +1,7 @@
 #!/usr/bin/env zsh
 
-dotfiles_home="~/dotfiles"
-
-if test -n "$(git -C ${dotfiles_home} status --porcelain)" ||
-   ! git -C ${dotfiles_home} diff --exit-code --stat --cached origin/main > /dev/null ; then
+if test -n "$(git -C ~/dotfiles status --porcelain)" ||
+   ! git -C ~/dotfiles diff --exit-code --stat --cached origin/main > /dev/null ; then
   echo -e "\e[36m=== DOTFILES IS DIRTY ===\e[m"
   echo -e "\e[33mThe dotfiles have been changed.\e[m"
   echo -e "\e[33mPlease update them with the following command.\e[m"
